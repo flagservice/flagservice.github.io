b470e28b434b822d064647c029bb1fe3e142bedd
flagservice <66883910+flagservice@users.noreply.github.com>
2020-06-13 13:59:06 -0500

    Angular JS

---
+++ MainController.js
@@ -0,0 +1,8 @@
+app.controller('MainController',['$scope', function($scope){
+    $scope.title = "Flag Service";
+    $scope.contact = {
+        name: "Ella Kinney",
+        email: "ellakinney716@gmail.com",
+        phone: "(214)-770-3185"
+    };
+}]);
