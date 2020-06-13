9821cf723f288ead8f1d9737c74f488096f8cb1d
flagservice <66883910+flagservice@users.noreply.github.com>
2020-06-13 14:01:59 -0500

    Delete MainController.js

--- MainController.js
+++
@@ -1,8 +0,0 @@
-app.controller('MainController',['$scope', function($scope){
-    $scope.title = "Flag Service";
-    $scope.contact = {
-        name: "Ella Kinney",
-        email: "ellakinney716@gmail.com",
-        phone: "(214)-770-3185"
-    };
-}]);
